94c980e105833249e5536bd741da10ec447411df
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-02 12:53:01 +0300

    add nums

---
+++ FirstStepsInCoding/03.Nums/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03.Nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=20; i++)
+            {
+            Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
