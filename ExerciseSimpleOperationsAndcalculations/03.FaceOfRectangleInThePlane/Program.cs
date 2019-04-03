16000268814dbd0a2b1032afd553798f2885953f
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-03 15:28:56 +0300

    Add face of a rectangle in the plane

---
+++ ExerciseSimpleOperationsAndcalculations/03.FaceOfRectangleInThePlane/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03.FaceOfRectangleInThePlane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double a = Math.Abs(x1 - x2);
+            double b = Math.Abs(y1 - y2);
+            double p = (2 * (a + b));
+            double s = a * b;
+            Console.WriteLine("{0:f2}",s);
+            Console.WriteLine("{0:f2}", p);
+        }
+    }
+}
