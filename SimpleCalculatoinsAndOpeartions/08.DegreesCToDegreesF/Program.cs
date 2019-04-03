809a357e5136654d0ba7e7fa761b01e94625a857
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-03 13:39:31 +0300

    Add degrees C to degrees F

---
+++ SimpleCalculatoinsAndOpeartions/08.DegreesCToDegreesF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _08.DegreesCToDegreesF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = double.Parse(Console.ReadLine());
+            double f = (celsius * 9) / 5 + 32;
+            Console.WriteLine("{0:f2}",f);
+        }
+    }
+}
