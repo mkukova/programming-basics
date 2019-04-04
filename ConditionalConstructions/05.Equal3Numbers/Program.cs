6cde1582cd6a82ed31946e01801cdd15662b1158
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-04 13:02:18 +0300

    Add equal 3 numbers

---
+++ ConditionalConstructions/05.Equal3Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05.Equal3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            if(a==b && a==c && b==c)
+            { Console.WriteLine("yes"); }
+            else { Console.WriteLine("no"); }
+        }
+    }
+}
