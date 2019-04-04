16caf760f6e196233eb77334ff9703768bbd7d23
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-04 11:05:58 +0300

    Add Tailoring Workshop

---
+++ ExerciseSimpleOperationsAndcalculations/04.TailoringWorkshop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.TailoringWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int masi = int.Parse(Console.ReadLine());
+            double daljina = double.Parse(Console.ReadLine());
+            double shirina = double.Parse(Console.ReadLine());
+            double pokrivka = masi * (daljina + 2 * 0.30) * (shirina + 2 * 0.30);
+            double kare = masi * (daljina / 2) * (daljina / 2);
+            double cenaDolar = pokrivka * 7 + kare * 9;
+            double cenaLeva = cenaDolar * 1.85;
+            Console.WriteLine("{0:F2}",cenaDolar + " USD");
+            Console.WriteLine($"{cenaLeva:F2} BGN");
+        }
+    }
+}
