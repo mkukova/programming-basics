7fc16197d9dbbaaa3ce1fc5f6a84e2e9f8bdd8aa
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-04 13:26:42 +0300

    Add same words

---
+++ ConditionalConstructions/08.SameWords/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _08.SameWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            a = a.ToLower();
+            b = b.ToLower();
+            if(a==b) { Console.WriteLine("yes"); }
+            else { Console.WriteLine(" no"); }
+        }
+    }
+}
