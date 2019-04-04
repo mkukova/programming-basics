ec542d5651a9a49d047b7f864e119d8e992e239f
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-04 13:09:14 +0300

    Add number from 100 to 200

---
+++ ConditionalConstructions/06.NumberFrom100To200/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _06.NumberFrom100To200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if(a<100)
+            { Console.WriteLine("Less than 100"); }
+            else if(a>100 & a<200)
+            { Console.WriteLine("Between 100 and 200"); }
+            else { Console.WriteLine("Greater than 200"); }
+        }
+    }
+}
