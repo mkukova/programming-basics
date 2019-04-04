a3d6ffba6e0a056f7b9a1b61d7e3976c4d578e98
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-04 13:18:42 +0300

    Add guess the password

---
+++ ConditionalConstructions/07.GuessThePassword/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07.GuessThePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "s3cr3t!P@ssw0rd";
+            string b = Console.ReadLine();
+            if (a == b) {Console.WriteLine("Welcome"); }
+            else { Console.WriteLine(" Wrong password!"); }
+            
+        }
+    }
+}
