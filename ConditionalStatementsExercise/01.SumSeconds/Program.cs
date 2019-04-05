33e0e46d206f12739da52352427bf1a5ebd72e0d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-05 13:31:44 +0300

    Add Sum seconds

--- ConditionalStatementsExercise/01.SumSeconds/Program.cs
+++ ConditionalStatementsExercise/01.SumSeconds/Program.cs
@@ -6,13 +6,13 @@ namespace _01.SumSeconds
     {
         static void Main(string[] args)
         {
-            int first = int.Parse(Console.ReadLine());
-            int second = int.Parse(Console.ReadLine());
-            int third = int.Parse(Console.ReadLine());
-            int time = first + second + third;
-            int minutes = time / 60;
-            int seconds = time % 60;
-            if(seconds<10)
+            int timefirst = int.Parse(Console.ReadLine());
+            int timesecond = int.Parse(Console.ReadLine());
+            int timethird = int.Parse(Console.ReadLine());
+            int totaltime = timefirst + timesecond + timethird;
+            int minutes = totaltime / 60;
+            int seconds = totaltime % 60;
+            if (seconds < 10)
             {
                 Console.Write("{0}:0{1}", minutes, seconds);
             }
