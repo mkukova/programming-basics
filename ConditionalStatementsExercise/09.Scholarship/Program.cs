5fd781f8220463e045ef6aef3411a703486b7f53
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-05 13:21:10 +0300

    Add Conditional Statements - Exercise solution

---
+++ ConditionalStatementsExercise/09.Scholarship/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.Scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
