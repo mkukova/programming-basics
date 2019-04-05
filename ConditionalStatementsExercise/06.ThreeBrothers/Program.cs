2ad86f0825c1be389c34d713525bee8a71a076f3
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-05 15:31:12 +0300

    Add three brothers

--- ConditionalStatementsExercise/06.ThreeBrothers/Program.cs
+++ ConditionalStatementsExercise/06.ThreeBrothers/Program.cs
@@ -6,7 +6,22 @@ namespace _06.ThreeBrothers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double firstbrother = double.Parse(Console.ReadLine());
+            double secondbrother = double.Parse(Console.ReadLine());
+            double thirdbrother = double.Parse(Console.ReadLine());
+            double fathertime = double.Parse(Console.ReadLine());
+            double neededtime = 1 / ((1 / firstbrother) + (1 / secondbrother) + (1 / thirdbrother));
+            double timewithrest = neededtime * 1.15;
+            double lefttime = fathertime - timewithrest;
+            Console.WriteLine($"Cleaning time: {timewithrest:f2}");
+            if (lefttime >= 0)
+            {
+                Console.WriteLine($"Yes, there is a surprise - time left -> {lefttime:f0} hours.");
+            }
+            else
+            {
+                Console.WriteLine($"No, there isn't a surprise - shortage of time -> {Math.Ceiling(Math.Abs(lefttime)):f0} hours.");
+            }
         }
     }
 }
