8e2fa7a2822a8e14db595c7750102e28f8d6799e
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-05 15:04:56 +0300

    Add time + 15 minutes

--- ConditionalStatementsExercise/05.Time+15Minutes/Program.cs
+++ ConditionalStatementsExercise/05.Time+15Minutes/Program.cs
@@ -6,7 +6,20 @@ namespace _05.Time_15Minutes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int strathour = int.Parse(Console.ReadLine());
+            int stratminutes = int.Parse(Console.ReadLine());
+            int newtime = 15 + stratminutes + strathour * 60;
+            int finalhour = newtime / 60;
+            int finalminutes = newtime % 60;
+            if (finalhour >= 24)
+            {
+                finalhour = finalhour - 24;
+            }
+            if (finalminutes < 10)
+            {
+                Console.WriteLine($"{finalhour}:0{finalminutes}");
+            }
+            else { Console.WriteLine($"{finalhour}:{finalminutes}"); }
         }
     }
 }
