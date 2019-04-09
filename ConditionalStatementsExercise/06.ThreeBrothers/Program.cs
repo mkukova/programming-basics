457b1e750b9d973a5e03d09c5119cfa164558c9d
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-09 15:58:23 +0300

    Change three brothers

--- ConditionalStatementsExercise/06.ThreeBrothers/Program.cs
+++ ConditionalStatementsExercise/06.ThreeBrothers/Program.cs
@@ -10,17 +10,18 @@ namespace _06.ThreeBrothers
             double secondbrother = double.Parse(Console.ReadLine());
             double thirdbrother = double.Parse(Console.ReadLine());
             double fathertime = double.Parse(Console.ReadLine());
-            double neededtime = 1 / ((1 / firstbrother) + (1 / secondbrother) + (1 / thirdbrother));
+            double neededtime = 1 / (1 / firstbrother + 1 / secondbrother + 1 / thirdbrother);
             double timewithrest = neededtime * 1.15;
             double lefttime = fathertime - timewithrest;
-            Console.WriteLine($"Cleaning time: {timewithrest:f2}");
+            Console.WriteLine($"Cleaning time: {timewithrest:F2}");
             if (lefttime >= 0)
             {
-                Console.WriteLine($"Yes, there is a surprise - time left -> {lefttime:f0} hours.");
+                Console.WriteLine($"Yes, there is a surprise - time left -> {Math.Floor(lefttime):f0} hours.");
             }
             else
             {
-                Console.WriteLine($"No, there isn't a surprise - shortage of time -> {Math.Ceiling(Math.Abs(lefttime)):f0} hours.");
+                lefttime = Math.Abs(lefttime);
+                Console.WriteLine($"No, there isn't a surprise - shortage of time -> {Math.Ceiling(lefttime):f0} hours.");
             }
         }
     }
