fb7dd8ac1d70e2d4c9c29ecdb3ca3be1a9f9badb
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-10 15:17:45 +0300

    Add Nested Conditional Statements solution

---
+++ NestedConditionalStatements/02.SmallShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmallShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NestedConditionalStatements/04.FruitOrVegetable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04.FruitOrVegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
