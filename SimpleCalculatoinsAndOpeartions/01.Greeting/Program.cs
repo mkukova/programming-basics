5da8b311de80072b2e354e84202306a1ed4cae33
mkukova <49146098+mkukova@users.noreply.github.com>
2019-04-12 14:27:30 +0300

    Update Program.cs

--- SimpleCalculatoinsAndOpeartions/01.Greeting/Program.cs
+++ SimpleCalculatoinsAndOpeartions/01.Greeting/Program.cs
@@ -7,7 +7,7 @@ namespace _01.Greeting
         static void Main(string[] args)
         {
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name );
+            Console.WriteLine($"Hello {name}");
         }
     }
 }
