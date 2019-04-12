79ff2ea064fb8d983e17fe4fdc25613dba55a8ad
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-12 14:43:33 +0300

    Add ciname project

--- NestedConditionalStatementsExercise/02.Cinema/Program.cs
+++ NestedConditionalStatementsExercise/02.Cinema/Program.cs
@@ -6,7 +6,23 @@ namespace _02.Cinema
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string type = Console.ReadLine().ToLower();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+            double income = 0.0;
+            if (type == "premiere")
+            {
+                income = rows * colums * 12.00;
+            }
+            else if (type == "normal")
+            {
+                income = rows * colums * 7.50;
+            }
+            else if (type == "discount")
+            {
+                income = rows * colums * 5.00;
+            }
+            Console.WriteLine($"{income:F2} leva");
         }
     }
 }
