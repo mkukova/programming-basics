01191d62e50fd465a9e74938b372fcb85517859f
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-12 14:29:17 +0300

    Add point on rectangle border project

--- NestedConditionalStatementsExercise/01.PointOnRectangleBorder/Program.cs
+++ NestedConditionalStatementsExercise/01.PointOnRectangleBorder/Program.cs
@@ -6,7 +6,22 @@ namespace _01.PointOnRectangleBorder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            bool firstcondition = (x == x1 || x == x2) && (y >= y1 && y <= y2);
+            bool secondcondition = (x >= x1 && x <= x2) && (y == y1 || y == y2);
+            if (firstcondition || secondcondition)
+            {
+                Console.WriteLine("Border");
+            }
+            else
+            {
+                Console.WriteLine("Inside / Outside");
+            }
         }
     }
 }
