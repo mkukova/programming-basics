2bac297ba630d9a1dab5cd53274e2c19c663e7aa
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-15 15:11:21 +0300

    Format code

--- ConditionalConstructions/11.AnimalType/Program.cs
+++ ConditionalConstructions/11.AnimalType/Program.cs
@@ -9,11 +9,16 @@ namespace _11.AnimalType
             string animal = Console.ReadLine();
             switch (animal)
             {
-                case "dog": Console.WriteLine("mammal"); break;
-                case "crocodile": Console.WriteLine("reptile"); break;
-                case "tortoise": Console.WriteLine("reptile"); break;
-                case "snake": Console.WriteLine("reptile"); break;
-                default: Console.WriteLine("unknown"); break;
+                case "dog":
+                    Console.WriteLine("mammal"); break;
+                case "crocodile":
+                    Console.WriteLine("reptile"); break;
+                case "tortoise":
+                    Console.WriteLine("reptile"); break;
+                case "snake":
+                    Console.WriteLine("reptile"); break;
+                default:
+                    Console.WriteLine("unknown"); break;
             }
         }
     }
