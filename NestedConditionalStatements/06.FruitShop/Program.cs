72225637d89713db514b61ad93541fa35d289e57
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-15 16:06:20 +0300

    Change fruit shop solution

--- NestedConditionalStatements/06.FruitShop/Program.cs
+++ NestedConditionalStatements/06.FruitShop/Program.cs
@@ -10,69 +10,72 @@ namespace _06.FruitShop
             string day = Console.ReadLine().ToLower();
             double quantity = double.Parse(Console.ReadLine());
             double price = 0;
-            if (day == "monday" || day == "tuesday" || day == "wednesday" || day == "thursday" || day == "friday")
+            switch (day)
             {
-                if (friut == "banana")
-                {
-                    price = 2.5;
-                }
-                else if (friut == "kiwi")
-                {
-                    price = 2.7;
-                }
-                else if (friut == "apple")
-                {
-                    price = 1.2;
-                }
-                else if (friut == "orange")
-                {
-                    price = 0.85;
-                }
-                else if (friut == "grapefruit")
-                {
-                    price = 1.45;
-                }
-                else if (friut == "pineapple")
-                {
-                    price = 5.5;
-                }
-                else if (friut == "grapes")
-                {
-                    price = 3.85;
-                }
+                case "monday":
+                case "tuesday":
+                case "wednesday":
+                case "thursday":
+                case "friday":
+                    switch (friut)
+                    {
+                        case "banana":
+                            price = 2.5;
+                            break;
+                        case "kiwi":
+                            price = 2.7;
+                            break;
+                        case "apple":
+                            price = 1.2;
+                            break;
+                        case "orange":
+                            price = 0.85;
+                            break;
+                        case "grapefruit":
+                            price = 1.45;
+                            break;
+                        case "pineapple":
+                            price = 5.5;
+                            break;
+                        case "grapes":
+                            price = 3.85;
+                            break;
+                    }
 
+                    break;
+                case "sunday":
+                case "saturday":
+                    switch (friut)
+                    {
+                        case "banana":
+                            price = 2.7;
+                            break;
+                        case "kiwi":
+                            price = 3.0;
+                            break;
+                        case "apple":
+                            price = 1.25;
+                            break;
+                        case "orange":
+                            price = 0.9;
+                            break;
+                        case "grapefruit":
+                            price = 1.6;
+                            break;
+                        case "pineapple":
+                            price = 5.6;
+                            break;
+                        case "grapes":
+                            price = 4.2;
+                            break;
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
             }
-            else if (day == "sunday" || day == "saturday")
-            {
-                if (friut == "banana")
-                {
-                    price = 2.7;
-                }
-                else if (friut == "kiwi")
-                {
-                    price = 3;
-                }
-                else if (friut == "apple")
-                {
-                    price = 1.25;
-                }
-                else if (friut == "orange")
-                {
-                    price = 0.9;
-                }
-                else if (friut == "grapefruit")
-                {
-                    price = 1.6;
-                }
-                else if (friut == "pineapple")
-                {
-                    price = 5.6;
-                }
-                else if (friut == "grapes")
-                {
-                    price = 4.2;
-                }
-            }
+
             if (price > 0)
             {
                 double sum = price * quantity;
