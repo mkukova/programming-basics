1b1d48b8ec13f6e2de495197b137950243efc1be
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-15 14:41:19 +0300

    Add fishing boat project

--- NestedConditionalStatementsExercise/05.FishingBoat/Program.cs
+++ NestedConditionalStatementsExercise/05.FishingBoat/Program.cs
@@ -6,7 +6,54 @@ namespace _05.FishingBoat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine().ToLower();
+            int fishers = int.Parse(Console.ReadLine());
+            double currentPrice = 0.0;
+            double discount = 0.0;
+            double finalPrice = 0.0;
+
+            switch (season)
+            {
+                case "spring":
+                    currentPrice = 3000; break;
+                case "summer":
+                    currentPrice = 4200; break;
+                case "autumn":
+                    currentPrice = 4200; break;
+                case "winter":
+                    currentPrice = 2600; break;
+            }
+
+            if (fishers <= 6)
+            {
+                discount = 0.1 * currentPrice;
+            }
+            else if (fishers >= 7 && fishers <= 11)
+            {
+                discount = 0.15 * currentPrice;
+            }
+            else
+            {
+                discount = 0.25 * currentPrice;
+            }
+
+            finalPrice = currentPrice - discount;
+
+            if ((fishers % 2 == 0) && (season != "autumn"))
+            {
+                finalPrice = finalPrice - (0.05 * finalPrice);
+            }
+
+            if (finalPrice <= budget)
+            {
+                Console.WriteLine($"Yes! You have {(budget - finalPrice):F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {(finalPrice - budget):F2} leva.");
+            }
+
         }
     }
 }
