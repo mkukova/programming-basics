44744eb24ef66439182d27dd0a1644781360c2c9
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-15 15:20:23 +0300

    Change personal titles

--- NestedConditionalStatements/01.PersonalTitles/Program.cs
+++ NestedConditionalStatements/01.PersonalTitles/Program.cs
@@ -8,27 +8,28 @@ namespace PersonalTitles
         {
             double age = double.Parse(Console.ReadLine());
             string gender = Console.ReadLine();
-            if(gender=="m")
+            switch (gender)
             {
-                if(age<16)
-                {
-                    Console.WriteLine("Master");
-                }
-                else
-                {
-                    Console.WriteLine("Mr.");
-                }
-            }
-            else
-            {
-                if (age < 16)
-                {
-                    Console.WriteLine("Miss");
-                }
-                else
-                {
-                    Console.WriteLine("Ms.");
-                }
+                case "m":
+                    if (age < 16)
+                    {
+                        Console.WriteLine("Master");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mr.");
+                    }
+                case "f":
+                    if (age < 16)
+                    {
+                        Console.WriteLine("Miss");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ms.");
+                    }
+                default:
+                    Console.WriteLine("Error");
             }
         }
     }
