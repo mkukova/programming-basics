08fb483f3efb8254ee563654f91f8aa5ef441b42
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-15 14:57:13 +0300

    Format code: add  new line

--- ConditionalConstructions/09.AreaOfFigures/Program.cs
+++ ConditionalConstructions/09.AreaOfFigures/Program.cs
@@ -2,38 +2,39 @@
 
 namespace _09.AreaOfFigures
 {
-        class Program
+    class Program
+    {
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            string figure = Console.ReadLine();
+            figure = figure.ToLower();
+            double area = 0.0;
+            switch (figure)
             {
-                string figure = Console.ReadLine();
-                figure = figure.ToLower();
-                double area = 0.0;
-                switch (figure)
-                {
-                    case "square":
-                        double a = double.Parse(Console.ReadLine());
-                        area = a * a;
-                        break;
-                    case "rectangle":
-                        double c = double.Parse(Console.ReadLine());
-                        double b = double.Parse(Console.ReadLine());
-                        area = c * b;
-                        break;
-                    case "circle":
-                        double r = double.Parse(Console.ReadLine());
-                        area = r * r * Math.PI;
-                        break;
-                    case "triangle":
-                        double d = double.Parse(Console.ReadLine());
-                        double h = double.Parse(Console.ReadLine());
-                        area = d * h / 2;
-                        break;
-                }
-                Console.WriteLine($"{area:f3} ");
+                case "square":
+                    double a = double.Parse(Console.ReadLine());
+                    area = a * a;
+                    break;
+                case "rectangle":
+                    double c = double.Parse(Console.ReadLine());
+                    double b = double.Parse(Console.ReadLine());
+                    area = c * b;
+                    break;
+                case "circle":
+                    double r = double.Parse(Console.ReadLine());
+                    area = r * r * Math.PI;
+                    break;
+                case "triangle":
+                    double d = double.Parse(Console.ReadLine());
+                    double h = double.Parse(Console.ReadLine());
+                    area = d * h / 2;
+                    break;
             }
+
+            Console.WriteLine($"{area:f3} ");
         }
-    
+    }
+
 }
-    
+
 
