64730caba834347b1c1398dfe515426303434cf0
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-15 14:48:27 +0300

    Format code

--- ConditionalConstructions/05.Equal3Numbers/Program.cs
+++ ConditionalConstructions/05.Equal3Numbers/Program.cs
@@ -9,8 +9,10 @@ namespace _05.Equal3Numbers
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
             double c = double.Parse(Console.ReadLine());
-            if(a==b && a==c && b==c)
-            { Console.WriteLine("yes"); }
+            if (a == b && a == c && b == c)
+            {
+                Console.WriteLine("yes");
+            }
             else { Console.WriteLine("no"); }
         }
     }
