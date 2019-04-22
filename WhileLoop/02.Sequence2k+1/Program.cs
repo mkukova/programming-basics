d6afa535a1f0589af148c02ae6e0817427354391
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-22 13:50:04 +0300

    Add sequence-2k+1 solution

--- WhileLoop/02.Sequence2k+1/Program.cs
+++ WhileLoop/02.Sequence2k+1/Program.cs
@@ -6,7 +6,13 @@ namespace _02.Sequence2k_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter <= number)
+            {
+                Console.WriteLine(counter);
+                counter = counter * 2 + 1;
+            }
         }
     }
 }
