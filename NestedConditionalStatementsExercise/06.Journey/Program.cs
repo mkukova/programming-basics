707719eaaa6cc0130fb9ce14ca1a1576970a83e2
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-22 15:09:30 +0300

    Change journey

--- NestedConditionalStatementsExercise/06.Journey/Program.cs
+++ NestedConditionalStatementsExercise/06.Journey/Program.cs
@@ -6,7 +6,50 @@ namespace _06.Journey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double spentAmount = 0.0;
+            if (budget <= 100)
+            {
+                switch (season)
+                {
+                    case "summer":
+                        spentAmount = 0.3 * budget;
+                        Console.WriteLine($"Somewhere in Bulgaria");
+                        Console.WriteLine($"Camp - {spentAmount:F2}");
+                        break;
+                    case "winter":
+                        spentAmount = 0.7 * budget;
+                        Console.WriteLine($"Somewhere in Bulgaria");
+                        Console.WriteLine($"Hotel - {spentAmount:F2}");
+                        break;
+                }
+            }
+            else if (budget > 100 && budget <= 1000)
+            {
+                switch (season)
+                {
+                    case "summer":
+                        spentAmount = 0.4 * budget;
+                        Console.WriteLine($"Somewhere in Balkans");
+                        Console.WriteLine($"Camp - {spentAmount:F2}");
+                        break;
+                    case "winter":
+                        spentAmount = 0.8 * budget;
+                        Console.WriteLine($"Somewhere in Balkans");
+                        Console.WriteLine($"Hotel - {spentAmount:F2}");
+                        break;
+                }
+
+            }
+            else
+            {
+                spentAmount = 0.9 * budget;
+                Console.WriteLine($"Somewhere in Europe");
+                Console.WriteLine($"Hotel - {spentAmount:F2}");
+
+            }
         }
     }
 }
+
