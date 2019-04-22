ad5987b7e5db76a6b6fa61791fda18303269c792
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-22 16:31:39 +0300

    Add exam-preparation

--- While-Loop-Exercise/02.ExamPreparation/Program.cs
+++ While-Loop-Exercise/02.ExamPreparation/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace _02.ExamPreparation
+namespace _01.OldBooks
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberBadGrades = int.Parse(Console.ReadLine());
+            int countBadGrades = 0;
+            int countGoodGrades = 0;
+            double sumGrades = 0.0;
+            string lastProblem = string.Empty;
+            double averageGrade = 0.0;
+            int totalProblems = 0;
+            while (countBadGrades < numberBadGrades)
+            {
+                string nameOfExercise = Console.ReadLine();
+                if (nameOfExercise == "Enough")
+                {
+                    Console.WriteLine($"Average score: {averageGrade}");
+                    Console.WriteLine($"Number of problems: {totalProblems}");
+                    Console.WriteLine($"Last problem: {lastProblem}");
+                    break;
+                }
+
+                int grade = int.Parse(Console.ReadLine());
+                sumGrades += grade;
+                countGoodGrades++;
+                if (grade <= 4.00)
+                {
+                    countBadGrades++;
+                }
+                else
+                {
+                    lastProblem = nameOfExercise;
+                }
+
+                totalProblems = countGoodGrades + countBadGrades;
+                averageGrade = sumGrades / totalProblems;
+            }
+
+            if (countBadGrades == numberBadGrades)
+            {
+                Console.WriteLine($"You need a break, {countBadGrades} poor grades.");
+            }
         }
     }
 }
