6ee6ba15257b54d94735463f1bb73ea6029952e0
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-22 15:43:50 +0300

    Add While-Loop-Exercise solution

---
+++ While-Loop-Exercise/01.OldBooks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
