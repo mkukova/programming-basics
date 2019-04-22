25f58fd1ff956fc815ea0908e235827f6697c1ee
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-22 14:24:00 +0300

    Add graduation solution

--- WhileLoop/06.Graduation/Program.cs
+++ WhileLoop/06.Graduation/Program.cs
@@ -6,7 +6,21 @@ namespace _06.Graduation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            int counter = 1;
+            double sum = 0.0;
+            while (counter <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade >= 4.00)
+                {
+                    sum += grade;
+                    counter++;
+                }
+            }
+
+            double average = sum / 12.0;
+            Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
         }
     }
 }
