3e8a3642999588c7e61e7f377d6ad303487fd72e
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-24 14:47:52 +0300

    Add walking

--- While-Loop-Exercise/04.Walking/Program.cs
+++ While-Loop-Exercise/04.Walking/Program.cs
@@ -6,7 +6,37 @@ namespace _04.Walking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int steps = 0;
+            string command = string.Empty;
+            int stepsDiff = 0;
+            while (steps < 10000)
+            {
+                command = Console.ReadLine();
+                if (command == "Going home")
+                {
+                    steps += int.Parse(Console.ReadLine());
+                    if (steps >= 10000)
+                    {
+                        Console.WriteLine("Goal reached! Good job!");
+                    }
+                    else
+                    {
+                        stepsDiff = 10000 - steps;
+                        Console.WriteLine($"{stepsDiff} more steps to reach goal.");
+
+                    }
+
+                    break;
+                }
+                else
+                {
+                    steps += int.Parse(command);
+                    if (steps >= 10000)
+                    {
+                        Console.WriteLine("Goal reached! Good job!");
+                    }
+                }
+            }
         }
     }
 }
