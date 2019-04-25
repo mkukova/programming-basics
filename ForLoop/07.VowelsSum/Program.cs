94f81f6e50f156c53ae4883febc6b263fa744a2f
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-25 15:39:28 +0300

    Add vowels-sum

--- ForLoop/07.VowelsSum/Program.cs
+++ ForLoop/07.VowelsSum/Program.cs
@@ -6,7 +6,34 @@ namespace _07.VowelsSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string word = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char letter = word[i];
+                switch (letter)
+                {
+                    case 'a':
+                        sum++;
+                        break;
+                    case 'e':
+                        sum += 2;
+                        break;
+                    case 'i':
+                        sum += 3;
+                        break;
+                    case 'o':
+                        sum += 4;
+                        break;
+                    case 'u':
+                        sum += 5;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
