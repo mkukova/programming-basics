c1596b43c5b867ef0ab04d510b1d32178a53300e
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-30 16:57:15 +0300

    Add equal-pairs

--- ForLoopExercise/04.EqualPairs/Program.cs
+++ ForLoopExercise/04.EqualPairs/Program.cs
@@ -6,7 +6,42 @@ namespace _04.EqualPairs
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n = int.Parse(Console.ReadLine());
+			double sum = 0;
+			double previousSum = 0;
+			double diff = 0;
+			double previousDiff = 0;
+			double maxDiff = 0;
+			for (int i = 0; i < n; i++)
+			{
+				double num1 = double.Parse(Console.ReadLine());
+				double num2 = double.Parse(Console.ReadLine());
+				sum = num1 + num2;
+				if (i == 0)
+				{
+					diff = 0;
+				}
+				else
+				{
+					diff =Math.Abs(sum - previousSum);
+					if (diff > previousDiff)
+					{
+						maxDiff = diff;
+					}
+				}
+				
+				previousSum = sum;				
+			}
+
+			switch(maxDiff)
+			{
+				case 0:
+					Console.WriteLine($"Yes, value={sum}");
+					break;
+				default:
+					Console.WriteLine($"No, maxdiff={maxDiff}");
+					break;
+			}
 		}
 	}
 }
