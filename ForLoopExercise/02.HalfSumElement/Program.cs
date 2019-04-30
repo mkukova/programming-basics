7053f0a879e5fe57778efd9ce8057d081044a7f3
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-04-30 15:42:38 +0300

    Add half-sum-element

--- ForLoopExercise/02.HalfSumElement/Program.cs
+++ ForLoopExercise/02.HalfSumElement/Program.cs
@@ -6,7 +6,29 @@ namespace _02.HalfSumElement
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n = int.Parse(Console.ReadLine());
+			int sum = 0;
+			int max = int.MinValue;
+			for (int i = 0; i < n; i++)
+			{
+				int num = int.Parse(Console.ReadLine());
+				if (num > max)
+				{
+					max = num;
+				}
+				sum += num;
+			}
+			sum = sum - max;
+			if (sum == max)
+			{
+				Console.WriteLine("Yes");
+				Console.WriteLine($"Sum = {sum}");
+			}
+			else
+			{
+				Console.WriteLine("No");
+				Console.WriteLine($"Diff = {Math.Abs(sum - max)}");
+			}
 		}
 	}
 }
