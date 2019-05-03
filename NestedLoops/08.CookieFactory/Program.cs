fcc67bf71373f270df21b52ab1404bd598a2d899
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-03 16:55:42 +0300

    Add cookie-factory

--- NestedLoops/08.CookieFactory/Program.cs
+++ NestedLoops/08.CookieFactory/Program.cs
@@ -6,7 +6,46 @@ namespace _08.CookieFactory
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int batchCount = int.Parse(Console.ReadLine());
+			for (int i = 1; i <= batchCount; i++)
+			{
+				bool flour = false;
+				bool eggs = false;
+				bool sugar = false;
+				string productName = Console.ReadLine();
+
+				while (true)
+				{
+					if (productName == "sugar")
+					{
+						sugar = true;
+					}
+					else if (productName == "eggs")
+					{
+						eggs = true;
+					}
+					else if (productName == "flour")
+					{
+						flour = true;
+					}
+					else if (productName == "Bake!")
+					{
+						if (sugar && eggs && flour)
+						{
+							Console.WriteLine($"Baking batch number {i}...");
+							break;
+						}
+						else
+						{
+							Console.WriteLine("The batter should contain flour, eggs and sugar!");
+						}
+					}
+
+					productName = Console.ReadLine();
+				}
+			}
 		}
 	}
 }
+
+
