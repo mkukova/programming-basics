7fe5fb3d3842a77f4b08c55ef21b0321eb45cdb2
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-03 14:45:59 +0300

    Add combination

--- NestedLoops/04.Combination/Program.cs
+++ NestedLoops/04.Combination/Program.cs
@@ -6,7 +6,29 @@ namespace _04.Combination
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n = int.Parse(Console.ReadLine());
+			int validCombinationsCount = 0;
+			for (int x1 = 0; x1 <= n; x1++)
+			{
+				for (int x2 = 0; x2 <= n; x2++)
+				{
+					for (int x3 = 0; x3 <= n; x3++)
+					{
+						for (int x4 = 0; x4 <= n; x4++)
+						{
+							for (int x5 = 0; x5 <= n; x5++)
+							{
+								if (x1 + x2 + x3 + x4 + x5 == n)
+								{
+									validCombinationsCount++;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			Console.WriteLine(validCombinationsCount);
 		}
 	}
 }
