666135c224a8e13130cbcbc184f70859833a8474
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-07 17:12:29 +0300

    Add number-pyramid

--- NestedLoopsExercise/02.NumberPyramid/Program.cs
+++ NestedLoopsExercise/02.NumberPyramid/Program.cs
@@ -6,7 +6,35 @@ namespace _02.NumberPyramid
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n = int.Parse(Console.ReadLine());
+			int currentValue = 1;
+			bool Stop = false;
+
+			for (int i = 1; i <= n; i++)
+			{
+				for (int j = 1; j <= i; j++)
+				{
+					if (currentValue > n)
+					{
+						Stop = true;
+						break;
+					}
+					else
+					{
+						Console.Write($"{currentValue} ");
+						currentValue++;
+					}
+				}
+
+				if (Stop)
+				{
+					break;
+				}
+				else
+				{
+					Console.WriteLine();
+				}
+			}
 		}
 	}
 }
