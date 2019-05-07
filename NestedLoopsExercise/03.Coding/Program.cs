348e4094f8f5a2be0212349c954b20a1e77af9bf
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-07 17:32:10 +0300

    Add coding

--- NestedLoopsExercise/03.Coding/Program.cs
+++ NestedLoopsExercise/03.Coding/Program.cs
@@ -6,7 +6,27 @@ namespace _03.Coding
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string number = Console.ReadLine();
+			int lenghtNumber = number.Length;
+
+			for (int i = lenghtNumber - 1; i >= 0; i--)
+			{
+				int currentDigit = int.Parse(number[i].ToString());
+				if (currentDigit == 0)
+				{
+					Console.WriteLine("ZERO");
+					continue;
+				}
+
+
+				for (int line = 1; line <= currentDigit; line++)
+				{
+
+					Console.Write($"{(char)(currentDigit + 33)}");
+				}
+
+				Console.WriteLine();
+			}
 		}
 	}
 }
