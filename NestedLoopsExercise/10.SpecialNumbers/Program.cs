cb4640484a7af0820b87ab2924ef1a72eb1fda55
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-14 17:21:57 +0300

    Add spacial-numbers

--- NestedLoopsExercise/10.SpecialNumbers/Program.cs
+++ NestedLoopsExercise/10.SpecialNumbers/Program.cs
@@ -6,7 +6,55 @@ namespace _10.SpecialNumbers
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int numberN = int.Parse(Console.ReadLine());
+			/*		for (int num1 = 1; num1 <= 9; num1++)
+					{
+						for (int num2 = 1; num1 <= 9; num2++)
+						{
+							for (int num3 = 1; num3 <= 9; num3++)
+							{
+								for (int num4 = 1; num4 <= 9; num4++)
+								{
+									if (numberN % num1 == 0 && numberN % num2 == 0 && numberN % num3 == 0 && numberN % num4 == 0)
+									{
+										Console.Write($"{num1}{num2}{num3}{num4} ");
+									}
+								}
+							}
+						}
+					}
+					*/
+			bool number = false;
+			for (int i = 1111; i <= 9999; i++)
+			{
+				int n = i;
+				while (n > 0)
+				{
+					if (n % 10 == 0)
+					{
+						number = false;
+						break;
+					}
+					else if (numberN % (n % 10) == 0)
+					{
+						number = true;
+					}
+					else if (numberN % (n % 10) != 0)
+					{
+						number = false;
+						break;
+					}
+
+					n = n / 10;
+				}
+
+				if (number)
+				{
+					Console.Write($"{i} ");
+				}
+
+				number = false;
+			}
 		}
 	}
 }
