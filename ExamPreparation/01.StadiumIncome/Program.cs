b498ff44c4a8a1f42858c2601523632fa9f6d410
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-15 16:53:27 +0300

    Format code

--- ExamPreparation/01.StadiumIncome/Program.cs
+++ ExamPreparation/01.StadiumIncome/Program.cs
@@ -9,9 +9,9 @@ namespace _01.StadiumIncome
 			int numberSectors = int.Parse(Console.ReadLine());
 			int capacity = int.Parse(Console.ReadLine());
 			double priceTicket = double.Parse(Console.ReadLine());
-			double totalIncome = capacity * priceTicket;			
+			double totalIncome = capacity * priceTicket;
 			double sectorIncome = totalIncome / numberSectors;
-			double charityMoney = (totalIncome - (sectorIncome*0.75))/8;
+			double charityMoney = (totalIncome - (sectorIncome * 0.75)) / 8;
 			Console.WriteLine($"Total income - {totalIncome:F2} BGN");
 			Console.WriteLine($"Money for charity - {charityMoney:F2} BGN");
 		}
