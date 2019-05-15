a4c97ed79944ffb65ebb464a9e055a9c5025e8dc
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-15 16:50:57 +0300

    Add football-kit

--- ExamPreparation/04.FootballKit/Program.cs
+++ ExamPreparation/04.FootballKit/Program.cs
@@ -6,7 +6,23 @@ namespace _04.FootballKit
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			double shirtPrice = double.Parse(Console.ReadLine());
+			double sumToReach = double.Parse(Console.ReadLine());
+			double shortsPrice = 0.75 * shirtPrice;
+			double socksPrice = 0.2 * shortsPrice;
+			double shoesPrice = 2 * (shortsPrice + shirtPrice);
+			double sum = shortsPrice + socksPrice + shoesPrice + shirtPrice;
+			double totalSum = sum - (0.15 * sum);
+			if (totalSum >= sumToReach)
+			{
+				Console.WriteLine($"Yes, he will earn the world-cup replica ball!");
+				Console.WriteLine($"His sum is {totalSum:F2} lv.");
+			}
+			else if (totalSum < sumToReach)
+			{
+				Console.WriteLine("No, he will not earn the world-cup replica ball.");
+				Console.WriteLine($"He needs {(sumToReach - totalSum):F2} lv. more.");
+			}
 		}
 	}
 }
