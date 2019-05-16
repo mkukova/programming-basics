dd821e668f2b34609264180a2277719fff1f9ee9
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-05-16 17:31:15 +0300

    Add substitute

--- ExamPreparation/11.Substitute/Program.cs
+++ ExamPreparation/11.Substitute/Program.cs
@@ -6,7 +6,39 @@ namespace _11.Substitute
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int firstNumberFirstDigit = int.Parse(Console.ReadLine());
+			int firstNumberSecondDigit = int.Parse(Console.ReadLine());
+			int secondNumberFirstDigit = int.Parse(Console.ReadLine());
+			int secondNumberSecondDigit = int.Parse(Console.ReadLine());
+			int counter = 6;
+			while (counter > 0)
+			{
+				for (int k = firstNumberFirstDigit; k <= 8; k++)
+				{
+					for (int l = 9; l >= firstNumberSecondDigit; l--)
+					{
+						for (int m = secondNumberFirstDigit; m <= 8; m++)
+						{
+							for (int n = 9; n >= secondNumberSecondDigit; n--)
+							{
+								if (firstNumberFirstDigit % 2 == 0 && secondNumberFirstDigit % 2 == 0 && firstNumberSecondDigit % 2 != 0 && secondNumberSecondDigit % 2 != 0)
+								{
+
+									if (firstNumberFirstDigit == secondNumberFirstDigit && firstNumberSecondDigit == secondNumberSecondDigit)
+									{
+										Console.WriteLine("Cannot change the same player.");
+									}
+									else
+									{
+										Console.WriteLine($"{firstNumberFirstDigit}{firstNumberSecondDigit} - {secondNumberFirstDigit}{secondNumberSecondDigit}");
+										counter--;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 }
