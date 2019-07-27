d05450a04c6a3d9a71ce5197f8f3bb4cd77d087e
Mariya Kukova <Mariya Kukova@MKUKOVA>
2019-07-27 10:24:30 +0300

    Add Exam-programming basic

--- MockExam/01.SeaTrip/Program.cs
+++ MockExam/01.SeaTrip/Program.cs
@@ -6,7 +6,29 @@ namespace _01.SeaTrip
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int hours = int.Parse(Console.ReadLine());
+			int minutes = int.Parse(Console.ReadLine());
+			int newHours = hours;
+			int newMinutes = minutes + 30;
+			while(newMinutes>60 && newHours>23)
+			{
+				newMinutes -= 60;
+				newHours += 1;
+				if (newHours > 23)
+				{
+					newHours -= 23;
+				}
+			}
+			
+			if(newMinutes<10)
+			{
+				Console.WriteLine($"{newHours}:0{newMinutes}");
+			}
+			else
+			{
+				Console.WriteLine($"{newHours}:{newMinutes}");
+			}
+			
 		}
 	}
 }
